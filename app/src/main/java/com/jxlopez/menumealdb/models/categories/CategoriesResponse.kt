b6c27b336d8ca027c3bd2464a926a845088a65c5
Administrador <jlopez@movilpyme.com>
2022-01-20 01:12:15 -0600

    - create models

---
+++ app/src/main/java/com/jxlopez/menumealdb/models/categories/CategoriesResponse.kt
@@ -0,0 +1,5 @@
+package com.jxlopez.menumealdb.models.categories
+
+data class CategoriesResponse(
+    val categories: List<Category>?=null
+)
---
+++ app/src/main/java/com/jxlopez/menumealdb/models/categories/Category.kt
@@ -0,0 +1,13 @@
+package com.jxlopez.menumealdb.models.categories
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Category(
+    val idCategory: String = "",
+    val strCategory: String = "",
+    val strCategoryThumb: String = "",
+    val strCategoryDescription: String = ""
+) : Parcelable
+
